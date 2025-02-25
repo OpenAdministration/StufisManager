b5ecc10f44f9aff88d0aa28a818dfa24dd4266ee
Lukas Staab <l.staab@posteo.de>
2025-02-25 15:09:34 +0100

    flux app template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,84 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
 
 
-        <!-- Scripts -->
-        @fluxAppearance
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <livewire:layout.navigation />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @fluxAppearance
+</head>
+<body class="min-h-screen bg-white dark:bg-zinc-800">
+<flux:sidebar sticky stashable class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
+    <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc." class="px-2 dark:hidden" />
+    <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc." class="px-2 hidden dark:flex" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-        @fluxScripts
-    </body>
-</html>
+    <flux:navlist variant="outline">
+        <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
+        <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
+        <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
+        <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
+
+        <flux:navlist.group expandable heading="Favorites" class="hidden lg:grid">
+            <flux:navlist.item href="#">Marketing site</flux:navlist.item>
+            <flux:navlist.item href="#">Android app</flux:navlist.item>
+            <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
+        </flux:navlist.group>
+    </flux:navlist>
+
+    <flux:spacer />
+
+    <flux:navlist variant="outline">
+        <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
+        <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
+    </flux:navlist>
+
+    <flux:dropdown position="top" align="start" class="max-lg:hidden">
+        <flux:profile avatar="https://fluxui.dev/img/demo/user.png" name="Olivia Martin" />
+
+        <flux:menu>
+            <flux:menu.radio.group>
+                <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
+                <flux:menu.radio>Truly Delta</flux:menu.radio>
+            </flux:menu.radio.group>
+
+            <flux:menu.separator />
+
+            <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+        </flux:menu>
+    </flux:dropdown>
+</flux:sidebar>
+
+<flux:header class="lg:hidden">
+    <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+    <flux:spacer />
+
+    <flux:dropdown position="top" alignt="start">
+        <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
+
+        <flux:menu>
+            <flux:menu.radio.group>
+                <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
+                <flux:menu.radio>Truly Delta</flux:menu.radio>
+            </flux:menu.radio.group>
+
+            <flux:menu.separator />
+
+            <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+        </flux:menu>
+    </flux:dropdown>
+</flux:header>
+
+<flux:main>
+    {{ $slot }}
+</flux:main>
+
+@fluxScripts
+</body>
