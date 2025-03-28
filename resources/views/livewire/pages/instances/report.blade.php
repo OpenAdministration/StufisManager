6f45692e2f69ca9de3c2ce73a1a1c9dea5e71b38
Lukas Staab <l.staab@posteo.de>
2025-03-28 18:20:08 +0100

    WIP Feature: new table for values

--- resources/views/livewire/pages/instances/report.blade.php
+++ resources/views/livewire/pages/instances/report.blade.php
@@ -3,6 +3,23 @@
     <flux:text class="mt-2">This is the standard text component for body copy and general content throughout your application.</flux:text>
 
     <flux:button wire:click="run()" icon="arrow-path-rounded-square">Run again</flux:button>
-
-    @dump($lastResult)
+    @isset($lastResult)
+        @if(!empty($lastResult->diff))
+            <flux:table>
+                <flux:table.columns>
+                    <flux:table.column>Key</flux:table.column>
+                    <flux:table.column>Target Value</flux:table.column>
+                    <flux:table.column>Actual Value</flux:table.column>
+                </flux:table.columns>
+                <flux:table.rows>
+                    @foreach($lastResult->diff as $key => $diff)
+                        <flux:table.row>{{ $key }}</flux:table.row>
+                        <flux:table.row>{{ $diff['target'] }}</flux:table.row>
+                        <flux:table.row>{{ $diff['actual'] }}</flux:table.row>
+                    @endforeach
+                </flux:table.rows>
+            </flux:table>
+            <flux:button>Fix it</flux:button>
+        @endif
+    @endisset
 </div>
